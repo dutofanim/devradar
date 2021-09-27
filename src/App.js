b4f2ddfa0935d68302d1589cc2b8ccf18947cee6
dutofanim <etofanim@gmail.com>
2021-09-27 05:17:25 -0300

    Add CSS style files.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from 'react';
+
+import './global.css';
+import './app.css';
+import './sidebar.css';
 
 function App() {
+
+
   return (
-    <>
-    <Header title="Dashboard 1"/>
-    <Header title="Dashboard 5"/>
-    <Header title="Dashboard 7"/>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+
+          <div className="input-block">
+            <label htmlFor="gitUserName">Github username</label>
+            <input name="gitUserName" id="gitUserName" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="userTechs">Techs</label>
+            <input name="userTechs" id="userTechs" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="gitUserLink">Github Link</label>
+            <input name="gitUserLink" id="gitUserLink" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
